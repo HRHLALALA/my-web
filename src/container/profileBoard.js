bf342042520774520c9569067f90003cd57adeb7
HRHLALALA <32263355+HRHLALALA@users.noreply.github.com>
2019-02-07 17:30:42 +1100

    add profile

---
+++ src/container/profileBoard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import JobInfo from './../components/Resume/JobInfo'
+class ProfilePage extends React.Component{
+    render(){
+        return(
+        <div>
+            <JobInfo />
+        </div>
+        );
+    }
+}
+export default ProfilePage;
