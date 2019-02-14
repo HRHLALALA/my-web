e8af9919374ea0d3ee509ad5d8fe6409e76820db
HRHLALALA <32263355+HRHLALALA@users.noreply.github.com>
2019-02-14 13:03:14 +1100

    add book component

---
+++ src/components/Book/Book.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Book extends React.Component{
+    
+
+
+
+}
+
+export default Book;
---
+++ src/pages/musicPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+class MusicPage extends React.Component{
+    render(){
+        return(
+            <div>
+
+            </div>
+        );
+    }
+
+
+
+
+
+
+}
