037e393c6d1643ce090643c652e6fe78df910c30
Renhao Huang <z5111342@ad.unsw.edu.au>
2019-04-01 00:42:32 +1100

    Updates

---
+++ precache-manifest.c86a816a4040167a8d130bcb130cee91.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "865f57aa6f294e0cc356",
+    "url": "/hrhlalala/static/css/main.85b3be92.chunk.css"
+  },
+  {
+    "revision": "865f57aa6f294e0cc356",
+    "url": "/hrhlalala/static/js/main.865f57aa.chunk.js"
+  },
+  {
+    "revision": "9ab687b88d87063bf4c4",
+    "url": "/hrhlalala/static/js/1.9ab687b8.chunk.js"
+  },
+  {
+    "revision": "6cae2c61106c17d66ec9",
+    "url": "/hrhlalala/static/js/runtime~main.6cae2c61.js"
+  },
+  {
+    "revision": "06918d24845c50cf8815ed350b10d7a5",
+    "url": "/hrhlalala/static/media/cat.06918d24.jpg"
+  },
+  {
+    "revision": "05de565e5414157d507fd4880c44dbd4",
+    "url": "/hrhlalala/static/media/starTrek.05de565e.jpg"
+  },
+  {
+    "revision": "a9be034fea1f4e8426ac837913055aa0",
+    "url": "/hrhlalala/static/media/facebook.a9be034f.png"
+  },
+  {
+    "revision": "ab3dfa6b5e0890fcce2f75525b1be742",
+    "url": "/hrhlalala/static/media/linkedin.ab3dfa6b.png"
+  },
+  {
+    "revision": "ef468e275a2c13d0bc7156549261fde6",
+    "url": "/hrhlalala/static/media/github.ef468e27.png"
+  },
+  {
+    "revision": "7111a953d9d0ab2547fc75e1c91829fe",
+    "url": "/hrhlalala/static/media/email.7111a953.png"
+  },
+  {
+    "revision": "adf90aa53ec95026295daa63dbee3438",
+    "url": "/hrhlalala/static/media/bgm.adf90aa5.mp3"
+  },
+  {
+    "revision": "125b830a6954377262fad2a48fa2deb9",
+    "url": "/hrhlalala/static/media/star1.125b830a.jpg"
+  },
+  {
+    "revision": "671b0d8a1779bab1c1bdd04c204a5b5a",
+    "url": "/hrhlalala/static/media/afternoon.671b0d8a.jpeg"
+  },
+  {
+    "revision": "a2444da2ca0a2d98f05ed9a48bccec9e",
+    "url": "/hrhlalala/static/media/basketball.a2444da2.jpg"
+  },
+  {
+    "revision": "db188470600346207dff29f8c4b8239e",
+    "url": "/hrhlalala/static/media/coffee.db188470.jpeg"
+  },
+  {
+    "revision": "4818a29f0c339e89d3065305ee9ce83a",
+    "url": "/hrhlalala/static/media/curve.4818a29f.jpeg"
+  },
+  {
+    "revision": "0dd989df83e17f4b7d6ce495655885c7",
+    "url": "/hrhlalala/static/media/garden.0dd989df.jpeg"
+  },
+  {
+    "revision": "b8ddcc547fb825ad379530b8ce190ea8",
+    "url": "/hrhlalala/static/media/lake.b8ddcc54.jpeg"
+  },
+  {
+    "revision": "d064b660f2deb9123a0cafdc8748b3fb",
+    "url": "/hrhlalala/static/media/ocean.d064b660.jpeg"
+  },
+  {
+    "revision": "05827025330e8ff2a27b540862a1493e",
+    "url": "/hrhlalala/static/media/ocean2.05827025.jpeg"
+  },
+  {
+    "revision": "fea70f13464a4ad4e0ff1df1346f29f6",
+    "url": "/hrhlalala/static/media/tuwu.fea70f13.jpeg"
+  },
+  {
+    "revision": "d4298aca1a8c0d4c5d355f4a7dd2d28b",
+    "url": "/hrhlalala/static/media/unsw.d4298aca.jpeg"
+  },
+  {
+    "revision": "077aab67214e8214712ca7db001d5ab2",
+    "url": "/hrhlalala/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6eecc81c24449172a5cf962a75977b4d.js"
+  "/hrhlalala/precache-manifest.c86a816a4040167a8d130bcb130cee91.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/hrhlalala/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
