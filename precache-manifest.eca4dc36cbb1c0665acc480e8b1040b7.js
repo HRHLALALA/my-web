52727c9915b02a538bdc4449008564c7e44edb2e
Renhao Huang <z5111342@ad.unsw.edu.au>
2019-04-01 02:31:26 +1100

    Updates

--- precache-manifest.eca4dc36cbb1c0665acc480e8b1040b7.js
+++ precache-manifest.eca4dc36cbb1c0665acc480e8b1040b7.js
@@ -1,94 +1,94 @@
 self.__precacheManifest = [
   {
     "revision": "53fb7f7990e12401f811",
-    "url": "./static/css/main.85b3be92.chunk.css"
+    "url": "/static/css/main.85b3be92.chunk.css"
   },
   {
     "revision": "53fb7f7990e12401f811",
-    "url": "./static/js/main.53fb7f79.chunk.js"
+    "url": "/static/js/main.53fb7f79.chunk.js"
   },
   {
     "revision": "e961f2038c92698ca2bf",
-    "url": "./static/js/1.e961f203.chunk.js"
+    "url": "/static/js/1.e961f203.chunk.js"
   },
   {
-    "revision": "4a686d48d5a089750c49",
-    "url": "./static/js/runtime~main.4a686d48.js"
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
   },
   {
     "revision": "06918d24845c50cf8815ed350b10d7a5",
-    "url": "./static/media/cat.06918d24.jpg"
+    "url": "/static/media/cat.06918d24.jpg"
   },
   {
     "revision": "05de565e5414157d507fd4880c44dbd4",
-    "url": "./static/media/starTrek.05de565e.jpg"
+    "url": "/static/media/starTrek.05de565e.jpg"
   },
   {
     "revision": "a9be034fea1f4e8426ac837913055aa0",
-    "url": "./static/media/facebook.a9be034f.png"
+    "url": "/static/media/facebook.a9be034f.png"
   },
   {
     "revision": "ab3dfa6b5e0890fcce2f75525b1be742",
-    "url": "./static/media/linkedin.ab3dfa6b.png"
+    "url": "/static/media/linkedin.ab3dfa6b.png"
   },
   {
     "revision": "ef468e275a2c13d0bc7156549261fde6",
-    "url": "./static/media/github.ef468e27.png"
+    "url": "/static/media/github.ef468e27.png"
   },
   {
     "revision": "7111a953d9d0ab2547fc75e1c91829fe",
-    "url": "./static/media/email.7111a953.png"
+    "url": "/static/media/email.7111a953.png"
   },
   {
     "revision": "adf90aa53ec95026295daa63dbee3438",
-    "url": "./static/media/bgm.adf90aa5.mp3"
+    "url": "/static/media/bgm.adf90aa5.mp3"
   },
   {
     "revision": "125b830a6954377262fad2a48fa2deb9",
-    "url": "./static/media/star1.125b830a.jpg"
+    "url": "/static/media/star1.125b830a.jpg"
   },
   {
     "revision": "671b0d8a1779bab1c1bdd04c204a5b5a",
-    "url": "./static/media/afternoon.671b0d8a.jpeg"
+    "url": "/static/media/afternoon.671b0d8a.jpeg"
   },
   {
     "revision": "a2444da2ca0a2d98f05ed9a48bccec9e",
-    "url": "./static/media/basketball.a2444da2.jpg"
+    "url": "/static/media/basketball.a2444da2.jpg"
   },
   {
     "revision": "db188470600346207dff29f8c4b8239e",
-    "url": "./static/media/coffee.db188470.jpeg"
+    "url": "/static/media/coffee.db188470.jpeg"
   },
   {
     "revision": "4818a29f0c339e89d3065305ee9ce83a",
-    "url": "./static/media/curve.4818a29f.jpeg"
+    "url": "/static/media/curve.4818a29f.jpeg"
   },
   {
     "revision": "0dd989df83e17f4b7d6ce495655885c7",
-    "url": "./static/media/garden.0dd989df.jpeg"
+    "url": "/static/media/garden.0dd989df.jpeg"
   },
   {
     "revision": "b8ddcc547fb825ad379530b8ce190ea8",
-    "url": "./static/media/lake.b8ddcc54.jpeg"
+    "url": "/static/media/lake.b8ddcc54.jpeg"
   },
   {
     "revision": "d064b660f2deb9123a0cafdc8748b3fb",
-    "url": "./static/media/ocean.d064b660.jpeg"
+    "url": "/static/media/ocean.d064b660.jpeg"
   },
   {
     "revision": "05827025330e8ff2a27b540862a1493e",
-    "url": "./static/media/ocean2.05827025.jpeg"
+    "url": "/static/media/ocean2.05827025.jpeg"
   },
   {
     "revision": "fea70f13464a4ad4e0ff1df1346f29f6",
-    "url": "./static/media/tuwu.fea70f13.jpeg"
+    "url": "/static/media/tuwu.fea70f13.jpeg"
   },
   {
     "revision": "d4298aca1a8c0d4c5d355f4a7dd2d28b",
-    "url": "./static/media/unsw.d4298aca.jpeg"
+    "url": "/static/media/unsw.d4298aca.jpeg"
   },
   {
-    "revision": "0ca8a456536a91b271e31733d0f2c3e7",
-    "url": "./index.html"
+    "revision": "a6d60b2629ae5706fd239a3001ec67c3",
+    "url": "/index.html"
   }
 ];
