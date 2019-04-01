4acfc1f979ded03388c9fcd391e0d1b1acced449
Renhao Huang <z5111342@ad.unsw.edu.au>
2019-04-01 23:04:21 +1100

    fix nav mobile fix

--- src/container/NavigationBar.js
+++ src/container/NavigationBar.js
@@ -9,6 +9,7 @@ class NavigationBar extends React.Component{
     render(){
         return(
             <div className = "container-NavBar">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
                  {   this.props.isLogo === "false"?
                 <Link className="Name HRH" to = "/home">
                     Renhao Huang
@@ -18,6 +19,13 @@ class NavigationBar extends React.Component{
                     UNSW
                 </a>
                  }
+                 <div id = "dropdown" onClick = {()=>this.dropdown()}>
+                    <li>  
+                        <a href = "javascript:void(0);"className="icon" >
+                            <i className="fa fa-bars" ></i>
+                        </a>
+                    </li> 
+                </div>
                 {   this.props.isLogo === "false"?
                     <div className = "nav">
                         <Navigation isHorizontal = "true" />
@@ -29,6 +37,17 @@ class NavigationBar extends React.Component{
                 }
             </div>
         )
+    }
+    dropdown(){
+        if(this.props.isLogo === "false")
+            var menu = document.getElementsByClassName("nav")[0];
+        else
+            var menu = document.getElementsByClassName("nav logo")[0];
+        var style = window.getComputedStyle(menu);
+        var display = style.getPropertyValue('display');
+        if(display === "none") menu.style.display = "block";
+        else menu.style.display = "none";
+
     }
 }
 
